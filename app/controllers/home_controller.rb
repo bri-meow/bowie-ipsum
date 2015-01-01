c69d2e492e0df4569cf7c3be8bd9af9ed53b1a84
Bri Dotson <bridotson@gmail.com>
2015-01-01 14:52:38 -0800

    refactor dictionary filling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,31 +1,28 @@
 class HomeController < ApplicationController
   def index
-    fetcher = Lyricfy::Fetcher.new
-    song = fetcher.search("David Bowie", "Starman")
-    markov = MarkyMarkov::TemporaryDictionary.new
-    song.lines.each do |line|
-      markov.parse_string(line)
-    end
-    @tagline = markov.generate_2_sentences
+    fill_dictionary
+    @tagline = $markov.generate_2_sentences
     @song = Song.new()
   end
 
   def generate_ipsum
-    # fill the dictionary
-    markov = MarkyMarkov::TemporaryDictionary.new
-    @songs = Song.all
-    @songs.each do |song|
-      fetcher = Lyricfy::Fetcher.new
-      lyrics = fetcher.search("David Bowie", song.title)
-      lyrics.lines.each do |line|
-        markov.parse_string(line)
-      end
-    end
     @song = Song.new()
     #return some ipsum
-    @ipsum = markov.generate_4_sentences
+    @ipsum = $markov.generate_4_sentences
     respond_to do |format|
       format.js {}
+    end
+  end
+
+  def fill_dictionary
+    $markov = MarkyMarkov::TemporaryDictionary.new
+    fetcher = Lyricfy::Fetcher.new
+    @songs = Song.all
+    @songs.each do |song|
+      lyrics = fetcher.search("David Bowie", song.title)
+      lyrics.lines.each do |line|
+        $markov.parse_string(line)
+      end
     end
   end
 end
