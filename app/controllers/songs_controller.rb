b6503c60bcb2b29a2d46cd9a5832eb3b59c191e4
Bri Dotson <bridotson@gmail.com>
2015-01-01 17:24:18 -0800

    add view for all songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,9 +2,16 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(params.require(:song).permit(:title))
     if @song.save
+      Ipsum.fill_dictionary
       redirect_to root_path, notice: "Added #{@song.title} to BowieIpsum dictionary!"
     else
       redirect_to root_path, notice: "There was a problem"
     end
+  end
+
+  def index
+    songs = Song.all
+    @songs_first = songs[0...songs.length/2]
+    @songs_last = songs[songs.length/2..-1]
   end
 end
